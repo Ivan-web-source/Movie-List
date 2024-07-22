82eeb48deae8b8a5c5e5a4bff8f2c236a6010d68
Ivan-web-source <132808076+Ivan-web-source@users.noreply.github.com>
2024-07-21 20:30:57 -0700

    add documentation on movie list

--- src/main/model/MovieCollection.java
+++ src/main/model/MovieCollection.java
@@ -7,27 +7,31 @@ public class MovieCollection {
     private List<Movie> movieList;
     private List<Movie> unwatchedList;
 
+    // EFFECTS: creates two empty movie list
     public MovieCollection() {
         movieList = new ArrayList<>();
         unwatchedList = new ArrayList<>();
     }
 
+    // REQUIRES: !movieList.contains(newMovie)
+    // MODIFIES: this
+    // EFFECTS: adds a movie into the list of movie
     public void addMovie(Movie newMovie) {
         movieList.add(newMovie);
     }
 
-    public void addUnwatchedMovie(Movie unwatchedMovie) {
-        movieList.add(unwatchedMovie);
-    }
-
+    // EFFECTS: returns a list of movie (watched and unwatched)
     public List<Movie> getMovieList() {
         return movieList;
     }
 
+    // EFFECTS: returns a list of unwatched movie
     public List<Movie> getUnwatchedList() {
         return unwatchedList;
     }
 
+    // MODIFIES: this
+    // EFFECTS: adds a movie into the list of unwatched movie
     public void viewUnwatched() {
         for (Movie currentMovie : movieList) {
             if (currentMovie.getWatchedStatus() == false) {
