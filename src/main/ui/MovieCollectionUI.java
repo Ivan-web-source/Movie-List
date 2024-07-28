db510033e3456dbf99d23acfe1446fd5d139257b
Ivan-web-source <132808076+Ivan-web-source@users.noreply.github.com>
2024-07-28 16:07:45 -0700

    customized intro gui and add image

--- src/main/ui/MovieCollectionUI.java
+++ src/main/ui/MovieCollectionUI.java
@@ -2,9 +2,20 @@ package ui;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
 
 import javax.swing.*;
 
+import model.Movie;
+import model.MovieCollection;
+import persistence.JsonReader;
+import persistence.JsonWriter;
+
+// Create a graphical interactive MovieCollection app that can add new movies,
+// see the list of unwatched movies, mark unwatched movies as watched movies,
+// and give a personal rating for their watched movies.
 public class MovieCollectionUI implements ActionListener {
     private JFrame frame;
     private JLabel introLabel;
@@ -14,7 +25,12 @@ public class MovieCollectionUI implements ActionListener {
     private JButton saveButton;
     private JButton loadButton;
     private JButton quitButton;
-    private JPanel panel;
+
+    private static final String JSON_STORE = "./data/myFile.json";
+    private JsonWriter jsonWriter;
+    private JsonReader jsonReader;
+    private MovieCollection movieList;
+    private JLabel imageLabel;
 
     /*
      * EFFECTS: handles user input on each action
@@ -25,49 +41,74 @@ public class MovieCollectionUI implements ActionListener {
      *          button 5 allows user to load movie list
      *          button 6 allows user to exit the application
      */
-    @SuppressWarnings({"checkstyle:MethodLength", "checkstyle:SuppressWarnings"})
     public MovieCollectionUI() {
+        init();
+
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        introLabel.setFont(new Font("Serif", Font.BOLD, 40));
+        topPanel.add(introLabel);
+
+        JPanel imagePanel = new JPanel();
+        imagePanel.add(imageLabel);
+
+        JPanel buttonPanel = new JPanel(new GridLayout(6, 1, 10, 10));
+        buttonPanel.add(addButton);
+        buttonPanel.add(viewButton);
+        buttonPanel.add(unwatchedButton);
+        buttonPanel.add(saveButton);
+        buttonPanel.add(loadButton);
+        buttonPanel.add(quitButton);
+
+        frame.setLayout(new BorderLayout());
+        frame.add(topPanel, BorderLayout.NORTH);
+        frame.add(imagePanel, BorderLayout.CENTER);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Movie Collection");
+        frame.pack();
+        frame.setVisible(true);
+
+        actionListener();
+    }
+
+    private void init() {
+        jsonWriter = new JsonWriter(JSON_STORE);
+        jsonReader = new JsonReader(JSON_STORE);
+        movieList = new MovieCollection("My List");
+
         frame = new JFrame();
-        introLabel = new JLabel("Welcome to your Movie Collection");
+        frame.setSize(550, 1200);
+        frame.setLocation(500, 100);
+        introLabel = new JLabel("Welcome to your Movie Collection!");
+        ImageIcon imageIcon = new ImageIcon("./img/MovieIcon.jpg");
+        imageLabel = new JLabel(imageIcon);
         addButton = new JButton("Add Movie");
         viewButton = new JButton("View Movie List");
         unwatchedButton = new JButton("View Unwatched Movies");
         saveButton = new JButton("Save Movie List");
         loadButton = new JButton("Load Movie List");
         quitButton = new JButton("Exit Application");
-        panel = new JPanel();
-
-        panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 30, 50));
-        panel.setLayout(new GridLayout(10, 10));
-
-        panel.add(introLabel);
-        panel.add(addButton);
-        panel.add(viewButton);
-        panel.add(unwatchedButton);
-        panel.add(saveButton);
-        panel.add(loadButton);
-        panel.add(quitButton);
-
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Movie Collection");
-        frame.pack();
-        frame.setVisible(true);
     }
 
     public void actionListener() {
         addButton.addActionListener(this);
         addButton.setActionCommand("AddMovie");
+
         viewButton.addActionListener(this);
-        addButton.setActionCommand("ViewList");
+        viewButton.setActionCommand("ViewList");
+
         unwatchedButton.addActionListener(this);
-        addButton.setActionCommand("ViewUnwatchedList");
+        unwatchedButton.setActionCommand("ViewUnwatchedList");
+
         saveButton.addActionListener(this);
-        addButton.setActionCommand("SaveList");
+        saveButton.setActionCommand("SaveList");
+
         loadButton.addActionListener(this);
-        addButton.setActionCommand("LoadList");
+        loadButton.setActionCommand("LoadList");
+
         quitButton.addActionListener(this);
-        addButton.setActionCommand("Exit");
+        quitButton.setActionCommand("Exit");
     }
 
     @Override
@@ -75,26 +116,19 @@ public class MovieCollectionUI implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         switch (e.getActionCommand()) {
             case "AddMovie":
-                frame.dispose();
                 addMovie();
                 break;
             case "ViewList":
-                frame.dispose();
                 viewList();
                 break;
             case "ViewUnwatchedList":
-                frame.dispose();
                 viewUnwatchedList();
                 break;
             case "SaveList":
                 saveList();
-                JOptionPane.showMessageDialog(null, "Data successfully saved!",
-                        "Save Data", JOptionPane.WARNING_MESSAGE);
                 break;
             case "LoadList":
                 loadList();
-                JOptionPane.showMessageDialog(null, "Data successfully loaded!",
-                        "Load Data", JOptionPane.WARNING_MESSAGE);
                 break;
             case "Exit":
                 frame.dispose();
@@ -102,28 +136,78 @@ public class MovieCollectionUI implements ActionListener {
         }
     }
 
-    private void loadList() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadList'");
+    @SuppressWarnings({"checkstyle:MethodLength", "checkstyle:SuppressWarnings"})
+    public void addMovie() {
+        JTextField titleField = new JTextField(5);
+        JTextField directorField = new JTextField(5);
+        JTextField genreField = new JTextField(5);
+        JTextField durationField = new JTextField(5);
+
+        JPanel myPanel = new JPanel();
+        myPanel.setLayout(new GridLayout(0, 2));
+        myPanel.add(new JLabel("Title:"));
+        myPanel.add(titleField);
+        myPanel.add(new JLabel("Director:"));
+        myPanel.add(directorField);
+        myPanel.add(new JLabel("Genre:"));
+        myPanel.add(genreField);
+        myPanel.add(new JLabel("Duration (mins):"));
+        myPanel.add(durationField);
+
+        int result = JOptionPane.showConfirmDialog(null, myPanel, 
+                 "Please Enter Movie Details", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            String title = titleField.getText();
+            String director = directorField.getText();
+            String genre = genreField.getText();
+            int duration = Integer.parseInt(durationField.getText());
+
+            Movie newMovie = new Movie(title, director, genre, duration);
+            movieList.addMovie(newMovie);
+
+            JOptionPane.showMessageDialog(null, "Movie successfully added!");
+        }
     }
 
-    private void saveList() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'saveList'");
+    public void viewList() {
+        List<Movie> movies = movieList.getMovieList();
+        StringBuilder movieListStr = new StringBuilder("Movie List:\n");
+
+        for (Movie movie : movies) {
+            movieListStr.append(movie.getTitle()).append("\n");
+        }
+
+        JOptionPane.showMessageDialog(null, movieListStr.toString());
     }
 
-    private void viewUnwatchedList() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'viewUnwatchedList'");
+    public void viewUnwatchedList() {
+        List<Movie> unwatchedMovies = movieList.getUnwatchedList();
+        StringBuilder unwatchedListStr = new StringBuilder("Unwatched Movies:\n");
+
+        for (Movie movie : unwatchedMovies) {
+            unwatchedListStr.append(movie.getTitle()).append("\n");
+        }
+
+        JOptionPane.showMessageDialog(null, unwatchedListStr.toString());
     }
 
-    private void viewList() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'viewList'");
+    public void saveList() {
+        try {
+            jsonWriter.open();
+            jsonWriter.write(movieList);
+            jsonWriter.close();
+            JOptionPane.showMessageDialog(null, "Data successfully saved!");
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Unable to write to file: " + JSON_STORE);
+        }
     }
 
-    private void addMovie() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addMovie'");
+    public void loadList() {
+        try {
+            movieList = jsonReader.read();
+            JOptionPane.showMessageDialog(null, "Data successfully loaded!");
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Unable to read from file: " + JSON_STORE);
+        }
     }
 }
