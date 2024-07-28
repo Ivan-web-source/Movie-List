360b2f60a1a03b01ed0c89cbfb2d4095996bc3b8
Ivan-web-source <132808076+Ivan-web-source@users.noreply.github.com>
2024-07-27 21:14:23 -0700

    fix checkstyle

--- src/main/ui/MovieCollectionUI.java
+++ src/main/ui/MovieCollectionUI.java
@@ -11,34 +11,45 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MovieCollectionUI implements ActionListener {
+    private JFrame frame;
+    private JButton addButton;
+    private JButton viewButton;
+    private JButton unwatchedButton;
+    private JButton saveButton;
+    private JButton loadButton;
+    private JPanel panel;
 
     public MovieCollectionUI() {
-        JFrame frame = new JFrame();
-        JButton addButton = new JButton("Add Movie");
-        JButton viewButton = new JButton("View Movie List");
-        JButton unwatchedButton = new JButton("View Unwatched Movies");
-        JButton saveButton = new JButton("Save Movie List");
-        JButton loadButton = new JButton("Load Movie List");
-        JPanel panel = new JPanel();
+        frame = new JFrame();
+        addButton = new JButton("Add Movie");
+        viewButton = new JButton("View Movie List");
+        unwatchedButton = new JButton("View Unwatched Movies");
+        saveButton = new JButton("Save Movie List");
+        loadButton = new JButton("Load Movie List");
+        panel = new JPanel();
 
         panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 30, 50));
         panel.setLayout(new GridLayout(10, 10));
+
         panel.add(addButton);
-        addButton.addActionListener(this);
         panel.add(viewButton);
-        viewButton.addActionListener(this);
         panel.add(unwatchedButton);
-        unwatchedButton.addActionListener(this);
         panel.add(saveButton);
-        saveButton.addActionListener(this);
         panel.add(loadButton);
-        loadButton.addActionListener(this);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Movie Collection");
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void actionListener() {
+        addButton.addActionListener(this);
+        viewButton.addActionListener(this);
+        unwatchedButton.addActionListener(this);
+        saveButton.addActionListener(this);
+        loadButton.addActionListener(this);
     }
 
     @Override
