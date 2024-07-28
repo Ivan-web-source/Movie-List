9dedbe483a1e0c9950777fcab658bf588c54b674
Ivan-web-source <132808076+Ivan-web-source@users.noreply.github.com>
2024-07-27 21:05:37 -0700

    create all buttons

--- src/main/ui/MovieCollectionUI.java
+++ src/main/ui/MovieCollectionUI.java
@@ -1,30 +1,48 @@
 package ui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class MovieCollectionUI {
+public class MovieCollectionUI implements ActionListener {
 
     public MovieCollectionUI() {
         JFrame frame = new JFrame();
-        JButton button = new JButton("Add Movie");
+        JButton addButton = new JButton("Add Movie");
+        JButton viewButton = new JButton("View Movie List");
+        JButton unwatchedButton = new JButton("View Unwatched Movies");
+        JButton saveButton = new JButton("Save Movie List");
+        JButton loadButton = new JButton("Load Movie List");
         JPanel panel = new JPanel();
 
-        panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
+        panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 30, 50));
         panel.setLayout(new GridLayout(10, 10));
-        panel.add(button);
+        panel.add(addButton);
+        addButton.addActionListener(this);
+        panel.add(viewButton);
+        viewButton.addActionListener(this);
+        panel.add(unwatchedButton);
+        unwatchedButton.addActionListener(this);
+        panel.add(saveButton);
+        saveButton.addActionListener(this);
+        panel.add(loadButton);
+        loadButton.addActionListener(this);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Movie Collection");
         frame.pack();
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
     }
 }
